55f1dd4ec235a32c3354289f57eb23a8473e66a9
arkangel6 <league@iMac-2.attlocal.net>
2018-12-10 19:54:14 -0800

    12/10/18

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,34 @@
 package _02_Chat_Application;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
-
+	String ip = "localhost";
+	int port = 8080;
+	
+	public void Client() {
+		try {
+	      Socket s = new Socket(ip, port);
+				DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+				dos.writeUTF("hiii");
+				DataInputStream dis = new DataInputStream(s.getInputStream());
+				System.out.println(dis.readUTF());
+				s.close();
+	   } catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void Server() {
+		
+	}
+	
+	
 }
